df027a11764eda4618c8d5e7a1bf536393ce8a98
devil1321 <s.dominik1321@gmail.com>
2023-12-06 14:37:03 +0100

    update

---
+++ src/context/works.js
@@ -0,0 +1,362 @@
+export const data = [{
+        category: 'UX/UI',
+        slides: ['applications/dashboard.jpg','websites/framework.jpg','e-commerce/mademe.jpg','e-commerce/sale-hub.png', 'applications/quiz-app.png', 'websites/portfolio.png', 'applications/todo-app-redux.png'],
+        articles: [{
+            node: {
+                title: 'DASH Company Admin Template',
+                subtitle: 'Next/Redux/TypeScript/SCSS',
+                text: 'This is my own Fullstack Company Admin Template. Regiser and Manage Your Company',
+                github: 'https://github.com/devil1321/life-dashboard-next-react-redux',
+                link:'https://life-dashboard-next-react-redux.vercel.app/'
+                
+            },
+        },{
+            node: {
+                title: 'Abstract Framework',
+                subtitle: 'HTML/SCSS/JS',
+                text: 'This is my own css and js framework Abstract JS. Framework in progress',
+                github: 'https://github.com/devil1321/abstract-framework',
+                link:'https://abstract-framework.netlify.app/'
+                
+            },
+        },{
+            node: {
+                title: 'Shop Mademe',
+                subtitle: 'Gatsby/Redux/TypeScript/SCSS/Contantful',
+                text: 'This is my own Fullstack Company Admin Template. Regiser and Manage Your Company',
+                github: 'https://bitbucket.org/devil1321/shop-mademe-final/src/master/',
+                link:'https://mademe-gatsby.netlify.app/'
+                
+            },
+        },{
+                node: {
+                    title: 'Sale Hub',
+                    subtitle: 'React/Redux/TypeScript/SCSS',
+                    text: 'This is a faithful and refreshed version of themeforest store template',
+                    github: 'https://github.com/devil1321/sale-hub-e-commerce-redux-typescript',
+                    link:'https://sale-hub-mine-copy.netlify.app'
+                    
+                },
+            },
+            {
+                node: {
+                    title: 'Quiz-App',
+                    subtitle: 'React/SCSS',
+                    text: 'Simple draggable and checkable quiz app',
+                    github: 'https://github.com/devil1321/quiz-app',
+                    link:'https://app.netlify.com/sites/quiz-app-mine'
+                },
+            },
+            {
+                node: {
+                    title: 'Portfolio',
+                    subtitle: 'Gatsby/SCSS',
+                    text: 'Portfolio builded with Gatsby',
+                    github: 'https://github.com/devil1321/portfolio-gatsby',
+                    link:'https://4web-dev-portfolio.netlify.app'
+                }
+            },
+            {
+                node: {
+                    title: 'Todo App Dates',
+                    subtitle: 'React/Redux/SCSS',
+                    text: 'Simple Todo List with dates and modify feature',
+                    github: 'https://github.com/devil1321/todo-app-redux',
+                    link:'https://todo-app-mine-redux.netlify.app/'
+                }
+            }
+        ]
+    },
+    {
+        category: 'Websites',
+        slides: ['websites/gatos.png','websites/music.jpg','websites/booking-clone.png','websites/youtube-clone.png','websites/chatex.png','websites/free-landing-page.png','websites/blog.png', 'websites/portfolio.png', 'websites/club-in.png', 'websites/easy-bank.png', 'websites/ice-and-cake.png', 'websites/manage-landing-page.png', 'websites/loopstudios.png', 'websites/apps-market.png', 'websites/rooms-homepage.png', ],
+        articles: [
+            {
+                node: {
+                    title: 'Gatos Website',
+                    subtitle: 'HTML/CSS/SCSS/JS/Gatsby/Gsap',
+                    text: 'This is a Modern Gatos Website',
+                    github: 'https://github.com/devil1321/gatos-website-gatsby',
+                    link:'https://gatos-website.netlify.app/'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Music SHOP',
+                    subtitle: 'HTML/CSS/SCSS/JS/Gatsby/React-Redux/Django/Python',
+                    text: 'This is a Fullstack Music Shop. Login test1321 Password test1321',
+                    github: 'https://github.com/devil1321/music-shop',
+                    link:'https://music-shop-ruby.vercel.app/login/'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Booking Clone',
+                    subtitle: 'HTML/CSS/SCSS/JS/Gatsby/React-Redux',
+                    text: 'Booking Clone made with Gatsby and React-Redux',
+                    github: 'https://github.com/devil1321/booking-clone/tree/master',
+                    link:'https://booking-clone-gatsby.netlify.app'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Youtube Clone',
+                    subtitle: 'HTML/CSS/SCSS/JS/TypeScript/React/React-Redux',
+                    text: 'Booking Clone made with React TypeScript and React-Redux',
+                    github: 'https://github.com/devil1321/youtube-clone-react-redux/tree/dev',
+                    link:'https://youtube-clone-redux.netlify.app'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Chatex Chat',
+                    subtitle: 'HTML/CSS/SCSS/JS/TypeScript/React/React-Redux/Gatsby',
+                    text: 'Simple Chat app using Gatsby',
+                    github: 'https://github.com/devil1321/chatex-gatsby',
+                    link:'https://chatex-worldwide.netlify.app/login/'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Free Landing Page',
+                    subtitle: 'HTML/CSS/SCSS/JS/SVG/GSAP',
+                    text: 'Free Landing Page site with animated SVG and gsap scroll trigger feature.',
+                    github: 'https://github.com/devil1321/free-landing-page',
+                    link:'https://free-landing-page-xd.netlify.app'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Blog Techy',
+                    subtitle: 'HTML/CSS/SCSS/JS/SVG/Gatsby/Contentful',
+                    text: 'Blog made with Gatsby/Contentful',
+                    github: 'https://github.com/devil1321/blog-techy-gatsby',
+                    link:'https://blog-technology-gatsby.netlify.app/'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Portfolio',
+                    subtitle: 'Gatsby/SCSS',
+                    text: 'Portfolio builded with Gatsby',
+                    github: 'https://github.com/devil1321/portfolio-gatsby',
+                    link:'https://4web-dev-portfolio.netlify.app'
+                }
+            },
+            {
+                node: {
+                    title: 'Club In',
+                    subtitle: 'HTML/CSS/SCSS/JS/XD',
+                    text: 'Club In Landing Page with carousels',
+                    github: 'https://github.com/devil1321/club-in-xd',
+                    link:'https://clubin-xd.netlify.app'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Easy Bank',
+                    subtitle: 'HTML/CSS/SCSS/JS',
+                    text: 'Easy Bank landing page from freeCodeCamp',
+                    github: 'https://github.com/devil1321/easybank-landing-page-master',
+                    link:'https://easy-bank-tutorial.netlify.app'
+
+                }
+
+            },
+            {
+                node: {
+                    title: 'Ice And Cake',
+                    subtitle: 'HTML/CSS/SCSS/JS/XD',
+                    text: 'Ice and Cake copy of a page from an Adobe XD template',
+                    github: 'https://github.com/devil1321/ice-cake',
+                    link:'https://ice-cake-adobe-xd.netlify.app'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Manage Landing Page',
+                    subtitle: 'HTML/CSS/SCSS/JS',
+                    text: 'Manage Landing Page from FrontEndMentor',
+                    github: 'https://github.com/devil1321/manage-landing-page-master',
+                    link:'https://manage-landing-page-front-end-mentor.netlify.app'
+                }
+
+            },
+            {
+                node: {
+                    title: 'LoopStudios',
+                    subtitle: 'HTML/CSS/SCSS/JS',
+                    text: 'Loop Studios Landing Page from FrontEndMentor',
+                    github: 'https://github.com/devil1321/loopstudios-landing-page',
+                    link:'https://loopstudios-front-end-mentor.netlify.app'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Apps Market',
+                    subtitle: 'HTML/CSS/SCSS/JS/XD',
+                    text: 'Apps Market Landing Page from Adobbe XD',
+                    github: 'https://github.com/devil1321/technology-landing-page-xd',
+                    link:'https://apps-market-xd.netlify.app'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Room Homepage',
+                    subtitle: 'HTML/CSS/SCSS/JS',
+                    text: 'Simple Room Home Page Landing Page',
+                    github: 'https://github.com/devil1321/room-homepage-master',
+                    link:'https://room-homepage-front-end-mentor.netlify.app'
+                }
+
+            }
+        ]
+    },
+    {
+        category: 'Applications',
+        slides: ['applications/dashboard.jpg','applications/wheather-app.png', 'applications/quiz-app.png','applications/canvas-paint-app.png', 'applications/todo-app-redux.png', 'applications/todo-draggable.png', 'applications/static-job-listing.png'],
+        articles: [{
+            node: {
+                title: 'DASH Company Admin Template',
+                subtitle: 'Next/Redux/TypeScript/SCSS',
+                text: 'This is my own Fullstack Company Admin Template. Regiser and Manage Your Company',
+                github: 'https://github.com/devil1321/life-dashboard-next-react-redux',
+                link:'https://life-dashboard-next-react-redux.vercel.app/'
+                
+            },
+        },{
+            node: {
+                title: 'Wheather App',
+                subtitle: 'HTML/CSS/SCSS/JS/React/React-Redux',
+                text: 'Wheather App Made With React React-Redux',
+                github: 'https://github.com/devil1321/wheather-app-redux',
+                link:'https://wheather-app-redux.netlify.app'
+            }
+
+        },
+            {
+                node: {
+                    title: 'Quiz-App',
+                    subtitle: 'React/SCSS',
+                    text: 'Simple draggable and checkable quiz app',
+                    github: 'https://github.com/devil1321/quiz-app',
+                    link:'https://quiz-app-mine.netlify.app'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Canvas Paint',
+                    subtitle: 'HTML/CSS/SCSS/JS',
+                    text: 'Canvas Paint App builded with JavaSript prototyping and canvas object',
+                    github: 'https://github.com/devil1321/canvas-paint',
+                    link:'https://canva-paint.netlify.app'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Todo App Dates',
+                    subtitle: 'React/Redux/SCSS',
+                    text: 'My simple todo app with option to modify and describes to do items',
+                    github: 'https://github.com/devil1321/todo-app-redux',
+                    link:'https://todo-app-mine-redux.netlify.app'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Todo App Draggable',
+                    subtitle: 'React/SCSS',
+                    text: 'Simple Todo List with draggable feature',
+                    github: 'https://github.com/devil1321/todo-app-main-react-with-draggable',
+                    link:'https://todo-app-main-react-draggable-front-end-mentor.netlify.app'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Static Job Listing',
+                    subtitle: 'React/SCSS',
+                    text: 'This is simple site with feature filtering jobs by badges',
+                    github: 'https://github.com/devil1321/static_job_listings_master_react_v1',
+                    link:'https://static-job-listings-front-end-mentor.netlify.app'
+                }
+
+            }
+        ]
+    },
+    {
+        category: 'E-commerce',
+        slides: ['e-commerce/mademe.jpg','e-commerce/sopa.png','e-commerce/household.png','e-commerce/shop-co.png','e-commerce/sale-hub.png', 'e-commerce/easy-shop.png'],
+        articles: [{
+            node: {
+                title: 'Shop Mademe',
+                subtitle: 'Gatsby/Redux/TypeScript/SCSS/Contantful',
+                text: 'This is my own Fullstack Company Admin Template. Regiser and Manage Your Company',
+                github: 'https://bitbucket.org/devil1321/shop-mademe-final/src/master/',
+                link:'https://mademe-gatsby.netlify.app/'                
+                }
+            },{
+                node: {
+                    title: 'Sopa Shop',
+                    subtitle: 'React/Redux/TypeScript/SCSS/Next',
+                    text: 'This is a Sopa Shop template',
+                    github: 'https://github.com/devil1321/sopa-shop-next',
+                    link:'https://sopa-shop-next.vercel.app/'
+                }
+
+            },
+            ,{
+                node: {
+                    title: 'House Hold Shop',
+                    subtitle: 'React/Redux/TypeScript/SCSS/Next',
+                    text: 'This is a Household Shop template',
+                    github: 'https://github.com/devil1321/house-hold-next',
+                    link:'https://household-next.vercel.app/'
+                }
+
+            }
+            ,{
+                node: {
+                    title: 'House Shop Co',
+                    subtitle: 'React/Redux/TypeScript/SCSS/Gatbsy',
+                    text: 'This is a Shop Co template',
+                    github: 'https://github.com/devil1321/shop-co-gatsby',
+                    link:'https://shop-co.netlify.app/'
+                }
+
+            },{
+                node: {
+                    title: 'Sale Hub',
+                    subtitle: 'React/Redux/TypeScript/SCSS',
+                    text: 'This is a faithful and refreshed version of themeforest store template',
+                    github: 'https://github.com/devil1321/sale-hub-e-commerce-redux-typescript',
+                    link:'https://sale-hub-mine-copy.netlify.app'
+                }
+
+            },
+            {
+                node: {
+                    title: 'Easy Shop',
+                    subtitle: 'React/SCSS',
+                    text: 'This is a simple shop builded with React',
+                    github: 'https://github.com/devil1321/react-smartphone-shop-v2',
+                    link:'https://my-test-store-v2.netlify.app'
+                }
+
+            }
+        ]
+    }
+]
